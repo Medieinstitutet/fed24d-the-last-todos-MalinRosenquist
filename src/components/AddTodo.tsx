4d3c0cb16ac49595e9d21fde1ab9762f6e6a609e
Malin Rosenquist <malinrosenquist@outlook.com>
2025-06-02 12:02:31 +0200

    feat: create a new todo based on form input

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,21 +1,60 @@
-import type { FormEvent } from "react";
+import { useState, type ChangeEvent, type FormEvent } from "react";
+import { Todo } from "../models/Todo";
 
 export const AddTodo = () => {
+  // Creates a new Todo object with default empty values
+  const [todo, setTodo] = useState<Todo>(new Todo("", "", false));
+
+  // Handles input changes for both text fields and checkbox.
+  // Updates the corresponding property in the todo state.
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.type === "text") {
+      // Update task or description based on input id
+      setTodo({ ...todo, [e.target.id]: e.target.value });
+    }
+
+    if (e.target.type === "checkbox") {
+      // Update completed status if input type is checkbox
+      setTodo({ ...todo, [e.target.id]: e.target.checked });
+    }
+  };
+
+  // Handles form submission, Currently prevents default form submit behavior
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
+
+  console.log(todo);
 
   return (
     <>
       <form className="grid grid-cols-2" onSubmit={handleSubmit}>
         <label htmlFor="">Task</label>
-        <input type="text" id="task" className="border px-2 py-1 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-200" />
+        <input
+          type="text"
+          id="task"
+          value={todo.task}
+          onChange={handleChange}
+          className="border px-2 py-1 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-200"
+        />
 
         <label htmlFor="">Description</label>
-        <input type="text" id="descripttion" className="border px-2 py-1 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-200" />
+        <input
+          type="text"
+          id="description"
+          value={todo.description}
+          onChange={handleChange}
+          className="border px-2 py-1 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-200"
+        />
 
         <label htmlFor="">Completed</label>
-        <input type="checkbox" id="completed" className="border px-2 py-1 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-200" />
+        <input
+          type="checkbox"
+          id="completed"
+          checked={todo.completed}
+          onChange={handleChange}
+          className="border px-2 py-1 border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-200"
+        />
 
         <button className="col-start-2">Add task</button>
       </form>
