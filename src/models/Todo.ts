0c8798849ab680471517b150b631d6d2ffac8e45
Malin Rosenquist <malinrosenquist@outlook.com>
2025-06-02 13:42:08 +0200

    refactor: improve id generation

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -4,7 +4,7 @@ export class Todo {
   description: string;
   completed: boolean;
   constructor(task: string, description: string, completed: boolean) {
-    this.id = Date.now();
+    this.id = Date.now() + Math.floor(Math.random() * 100);
     this.task = task;
     this.description = description;
     this.completed = completed;
