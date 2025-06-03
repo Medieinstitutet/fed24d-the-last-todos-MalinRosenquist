f15f9de2d60a0323f0c072cd936af702ae2d2eb8
Malin Rosenquist <malinrosenquist@outlook.com>
2025-06-03 13:48:38 +0200

    feat: save todos to local storage

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -7,13 +7,17 @@ import { Todos } from "./Todos";
 // Holds the state
 
 export const TodoApp = () => {
-  const [todos, setTodos] = useState<Todo[]>([
+  const defaultTodos = [
     new Todo("Plantera tomater", "High", "Sätt tomatplantor i växthuset", false),
     new Todo("Vattna blommor", "High", "Vattna blomrabatten på framsidan", true),
     new Todo("Rensa ogräs", "Low", "Rensa ogräs mellan odlingslådorna", false),
     new Todo("Gödsla grönsakslandet", "High", "Använd kompostjord och kogödsel", false),
     new Todo("Beskär äppelträdet", "Medium", "Ta bort döda grenar och forma kronan", true),
-  ]);
+  ];
+
+  const savedTodos = localStorage.getItem("todos");
+
+  const [todos, setTodos] = useState<Todo[]>(savedTodos ? JSON.parse(savedTodos) : defaultTodos);
 
   const addTodo = (t: Todo) => {
     setTodos([...todos, t]);
@@ -27,7 +31,9 @@ export const TodoApp = () => {
     setTodos(todos.filter((t) => t.id !== id));
   };
 
+  localStorage.setItem("todos", JSON.stringify(todos));
   console.log(todos);
+
   return (
     <>
       <AddTodo addTodo={addTodo} />
