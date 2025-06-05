6e729b941bcb2c55f11dfbb644e5364ae060a2ab
Malin Rosenquist <malinrosenquist@outlook.com>
2025-06-05 12:26:34 +0200

    chore: fix typo

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ export const Button = ({ children, className, onClick }: ButtonProps) => {
     <>
       <button
         onClick={onClick}
-        className={`px-5 py-2.5 border border-transparent rounded-lg text-white uppercase cursor-pointer font-semibold focus:outline-none transition-colors duration-250" focus:ring-3 focus:ring-blue-500 ${className}`}
+        className={`px-5 py-2.5 border border-transparent rounded-lg text-white uppercase cursor-pointer font-semibold focus:outline-none transition-colors duration-250 focus:ring-3 focus:ring-blue-500 ${className}`}
       >
         {children}
       </button>
