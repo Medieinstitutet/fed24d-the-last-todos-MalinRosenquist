c785fad49751d67410ef5b970a95c6c9136876ce
Malin Rosenquist <malinrosenquist@outlook.com>
2025-06-05 12:26:21 +0200

    style: update todo card layout and reorder elements for better UX

--- src/components/TodoView.tsx
+++ src/components/TodoView.tsx
@@ -14,7 +14,7 @@ export const TodoView = ({ todo, toggleCompleted, removeTodo }: TodoViewProps) =
   const priorityClasses = {
     high: "text-red-600 font-extrabold",
     medium: "text-orange-500 font-bold",
-    low: "text-yellow-700 font-semibold",
+    low: "text-blue-700 font-semibold",
   };
 
   console.log("Priority value:", todo.priority);
@@ -22,16 +22,24 @@ export const TodoView = ({ todo, toggleCompleted, removeTodo }: TodoViewProps) =
     <>
       <li key={todo.id}>
         <div
-          className={`border-2 rounded-l-4xl p-3 shadow-xl flex flex-col items-start space-y-2 hover:scale-105 transition, duration-400 
-            ${todo.completed ? "border-green-600 bg-[#8CF298]/30" : "border-gray-400"}`}
+          className={`border-2 rounded-l-4xl p-4 shadow-lg flex flex-col items-start space-y-2 hover:scale-105 transition, duration-400 
+            ${todo.completed ? "border-green-600 bg-[#8CF298]/30" : "border-gray-400 bg-white"}`}
         >
-          <h3 className="text-xl">{todo.title}</h3>
+          <h3 className="text-xl font-semibold">{todo.title}</h3>
+
+          <div>
+            <span className="mr-2">Task:</span>
+            <p className="text-left">{todo.description}</p>
+          </div>
+
           <div className="flex">
-            <p className="mr-2">Priority:</p>
+            <span className="mr-2">Priority:</span>
             <span className={`${priorityClasses[todo.priority]}`}>{todo.priority.charAt(0).toUpperCase() + todo.priority.slice(1)}</span>
           </div>
-          <p className="text-left">{todo.description}</p>
-          <p className={`${todo.completed ? "font-semibold" : "italic"}`}>{todo.completed ? "Completed" : "Not completed"}</p>
+          <div className="flex">
+            <span className="mr-2">Status:</span>
+            <p className={`${todo.completed ? "font-semibold" : "italic"}`}>{todo.completed ? "Completed" : "Not completed"}</p>
+          </div>
 
           <div className="w-full flex justify-center gap-x-3 p-3">
             <Button className="btn bg-[#14612E] hover:bg-[#197638] transition" onClick={() => toggleCompleted(todo.id)}>
