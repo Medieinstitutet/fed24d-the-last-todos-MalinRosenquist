c0631ac6ac074651446206586ce87ead65b70321
Malin Rosenquist <malinrosenquist@outlook.com>
2025-06-05 12:20:09 +0200

    style: replace css styling with Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import "./App.css";
 import { TodoApp } from "./components/TodoApp";
 
 function App() {
   return (
-    <>
+    <div className="p-8 min-w-[320px] bg-gray-50 min-h-screen font-[Urbanist,_sans-serif]">
+      <h1 className="font-[Happy_Monkey,_sans-serif] text-6xl text-center mb-6">Easy 2 do</h1>
       <TodoApp />
-    </>
+    </div>
   );
 }
 
