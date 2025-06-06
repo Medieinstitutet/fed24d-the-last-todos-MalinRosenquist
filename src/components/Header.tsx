98c1f8db4c0190ae4c0fdd95ca3681f029918d70
Malin Rosenquist <malinrosenquist@outlook.com>
2025-06-06 12:08:00 +0200

    chore: adjust header size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import featherImg from "../assets/feather.png";
 
 export const Header = () => {
   return (
-    <header className="flex flex-row justify-center items-center h-50 sm:h-62 md:h-70 bg-white px-4 gap-4 sm:gap-6">
+    <header className="flex flex-row justify-center items-center h-40 sm:h-46 md:h-58 bg-white px-4 gap-4 sm:gap-6">
       <img src={featherImg} alt="Yellow feather" className="h-35 sm:h-40 md:h-50 w-auto rotate-6 object-contain" />
       <h1 className="text-green-900 text-5xl sm:text-7xl md:text-8xl font-[Oregano,_sans-serif]">Easy 2 Do</h1>
     </header>
